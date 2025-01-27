577585c6fe9de4f44392874a81f2926b1e8a7292
Pavel Zorin <sistpavel@gmail.com>
2025-01-27 10:43:20 +1000

    Registration done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import './App.css';
-import AboS from './Components/AboutUs_pg/AboutUs.jsx';
 import Registration from './Components/Registration/RegForm';
 
-function App() {
-  return (
-    <div> <AboS/>
-    </div>
-  );
-}
 
 function App() {
-  return (
-    <div> <Registration/>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <div> <Registration/>
+      </div>
+    );
+  }
+  
+  export default App;
