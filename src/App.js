f9b595900ea5d882760d3f02caa23b92f95429c8
Pavel Zorin <sistpavel@gmail.com>
2025-01-27 10:25:31 +1000

    merged

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-<<<<<<< HEAD
 import AboS from './Components/AboutUs_pg/AboutUs.jsx';
-
-function App() {
-  return (
-    <div> <AboS/>
-=======
 import Registration from './Components/Registration/RegForm';
 
 function App() {
   return (
+    <div> <AboS/>
+    </div>
+  );
+}
+
+function App() {
+  return (
     <div> <Registration/>
->>>>>>> 7889b0ab7e6c4fd184a33a87198e54a326536648
     </div>
   );
 }
