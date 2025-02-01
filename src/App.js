f284db5b82dbd126598b3290476902edffaf8121
s7meru <lokyclose@gmail.com>
2025-02-01 11:24:58 +1000

    fix regform

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Registration from './Components/Registration/RegForm';
-
+import Login from './Components/Login/LoginForm';
 function App() {
   return (
-    <div> <Registration/>
+    <div> <Login/>
     </div>
   );
 }
