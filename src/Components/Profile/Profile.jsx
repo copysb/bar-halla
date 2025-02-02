b0e39b75c13fe6236ae505fc0d6ca9948637f996
s7meru <lokyclose@gmail.com>
2025-02-03 00:54:18 +1000

    trying fix profile

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -12,7 +12,7 @@ const Profile = () => {
       <nav>
           <div className='left-side'>
               <ul>
-                <li><FaUser/></li>
+                <li><FaUser className='icon'/></li>
                 <li>Profile</li>
               </ul>
               <ul className='tays'>
@@ -28,21 +28,22 @@ const Profile = () => {
               </ul>
             </div>
           </div>
-
-          <span className='right-side'>
-              <div className='func-border1'></div>
-              <div className='func-border2'>
-                <li className='TL'>Turn lights?</li>
-                <li>
-                  <button><svg className='dark-theme' width="189" height="76" viewBox="0 0 189 76" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect x="1.5" y="1.5" width="186" height="73" rx="36.5" fill="#262626" stroke="#5A5A5A" stroke-width="3" />
-                    <circle cx="148" cy="38" r="29" fill="#D9D9D9" />
-                  </svg></button>
-                </li>
-              </div>
-              <div className='func-border3'></div>
-          </span>
       </nav>
+      <span className='right-side'>
+        <div className='func-border1'></div>
+          <div className='func-border2'>
+            <li className='TL'>Turn lights?</li>
+            <li>
+              <button>
+                <svg className='dark-theme' width="189" height="76" viewBox="0 0 189 76" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="1.5" y="1.5" width="186" height="73" rx="36.5" fill="#262626" stroke="#5A5A5A" stroke-width="3" />
+                  <circle cx="148" cy="38" r="29" fill="#D9D9D9" />
+                </svg>
+              </button>
+            </li>
+          </div>
+        <div className='func-border3'></div>
+      </span>
       <div className='foot'><Footer/></div>
     </div>
   )
