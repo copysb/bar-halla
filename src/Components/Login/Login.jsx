ece31318ea6f3c5e3f6d1a4469607fc7e5097def
Pavel Zorin <sistpavel@gmail.com>
2025-02-03 11:53:20 +1000

    ALL DONE

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,20 +3,23 @@ import './Login.css';
 
 const Login = () => {
     return (
-        <div className='wrapper'>
-            <from action = "">
-                <h1>LOGIN</h1>
-                <div className = "input-box">
-                    <input type="text"  placeholder= 'Username...' required/>
-                </div>
-                <div className = "input-box">
-                    <input type="password"  placeholder= 'Password...' required/>
-                </div>
-                <div className="forgot">
-                    <a href="*">Forgot password?</a>
-                </div>
-                <a className='mpage' href='/main' type="submit">CONFIRM</a>
-            </from>
+        <div>
+            <h1>SIGN IN</h1>
+            <div className='wrapper loginp'>
+                <from action = "">
+
+                    <div className = "input-box">
+                        <input type="text"  placeholder= 'Username...' required/>
+                    </div>
+                    <div className = "input-box">
+                        <input type="password"  placeholder= 'Password...' required/>
+                    </div>
+                    <div className="check-account">
+                        <a href="*" className="nextp">Forgot password?</a>
+                    </div>
+                    <a className='mpage' href='/main' type="submit">CONFIRM</a>
+                </from>
+            </div>
         </div>
     );
 };
