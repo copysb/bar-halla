e0c5cd8e0b0bf4b25fbf0524c63b48458e219a8f
Pavel Zorin <sistpavel@gmail.com>
2025-02-03 12:00:18 +1000

    All DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import MainPage from './Components/MainPage/MainPage.jsx'
 import Profile from './Components/Profile/Profile.jsx'
 import Oops from './Components/Oops/Oops.jsx'
